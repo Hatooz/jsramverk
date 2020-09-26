69f9d65eee626c637e11f62db77a88af4c4a469f
Hatooz <hatem.mohieldin@gmail.com>
2020-09-26 15:25:42 +0200

    kmom02 backend first commit. SQLite working and static info is being fetched from it using axios on frontend.

--- backend/app.js
+++ backend/app.js
@@ -1,94 +1,20 @@
 const express = require("express");
-const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require("body-parser");
-const index = require('./routes/index');
-const hello = require('./routes/hello');
-const port = 1337;
-app.use(cors());
-// don't show the log when it is test
-if (process.env.NODE_ENV !== 'test') {
-    // use morgan to log at command line
-    app.use(morgan('combined')); // 'combined' outputs the Apache style LOGs
-}
-app.use(bodyParser.json()); // for parsing application/json
+
+const app = express();
+
+//Middleware
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cors());
 
+const index = require('./routes/api/index');
 
-// This is middleware called for all routes.
-// Middleware takes three parameters.
-app.use((req, res, next) => {
-    console.log(req.method);
-    console.log(req.path);
-    next();
-});
+app.use('/', index)
 
-app.use('/', index);
-app.use('/hello', hello);
-// Add routes for 404 and error handling
-// Catch 404 and forward to error handler
-// Put this last
-app.use((req, res, next) => {
-    var err = new Error("Not Found");
-    err.status = 404;
-    next(err);
-});
+const port = 1337;
 
-app.use((err, req, res, next) => {
-    if (res.headersSent) {
-        return next(err);
-    }
-
-    res.status(err.status || 500).json({
-        "errors": [
-            {
-                "status": err.status,
-                "title":  err.message,
-                "detail": err.message
-            }
-        ]
-    });
-});
-
-// Add a route
-// Testing routes with method
-app.get("/user", (req, res) => {
-    res.json({
-        data: {
-            msg: "Got a GET request, sending back default 200"
-        }
-    });
-});
-app.get("/hello/:msg", (req, res) => {
-    const data = {
-        data: {
-            msg: req.params.msg
-        }
-    };
-
-    res.json(data);
-});
-
-
-app.post("/user", (req, res) => {
-    res.status(201).json({
-        data: {
-            msg: "Got a POST request, sending back 201 Created"
-        }
-    });
-});
-
-app.put("/user", (req, res) => {
-    // PUT requests should return 204 No Content
-    res.status(204).send();
-});
-
-app.delete("/user", (req, res) => {
-    // DELETE requests should return 204 No Content
-    res.status(204).send();
-});
-
-
-// Start up server
 app.listen(port, () => console.log(`Example API listening on port ${port}!`));
+
