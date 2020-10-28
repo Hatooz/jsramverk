7191b6d1d106fad70f40e4a90abce259a9c6c267
Hatooz <hatem.mohieldin@gmail.com>
2020-10-28 22:21:02 +0100

    kmom04 second commit. travis.yml was not added to last commit. adding it here

---
+++ test/frontendTest.js
@@ -0,0 +1,54 @@
+
+const assert = require("assert");
+const test = require("selenium-webdriver/testing");
+const webdriver = require("selenium-webdriver");
+const By = webdriver.By;
+
+let browser;
+
+
+// Does not work with WSL!! Use cygwin
+
+
+
+// Test suite
+test.describe("Multipage", function() {
+    test.beforeEach(function(done) {
+        this.timeout(20000);
+        browser = new webdriver.Builder().
+            withCapabilities(webdriver.Capabilities.firefox()).build();
+
+        browser.get("http://localhost:8080/#/");
+        done();
+    });
+
+    test.afterEach(function(done) {
+        browser.quit();
+        done();
+    });
+
+
+    function goToNavLink(target) {
+        browser.findElement(By.linkText(target)).then(function(element) {
+            element.click();
+        });
+    }
+
+    function matchUrl(target) {
+        browser.getCurrentUrl().then(function(url) {
+            assert.ok(url.endsWith("/" + target));
+        });
+    }
+
+
+    test.it("Test go to Home", function(done) {
+        // try use nav link
+        goToNavLink("Register");
+
+        // assertH1("Register");
+        matchUrl("register" );
+
+        done();
+    });
+
+})
