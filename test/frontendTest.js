d842c2fc6624917a54bef980dcbc6e87a7ff1313
Hatooz <hatem.mohieldin@gmail.com>
2020-10-29 20:33:07 +0100

    kmom04 could this be?

--- test/frontendTest.js
+++ test/frontendTest.js
@@ -1,6 +1,7 @@
 const assert = require("assert");
 const test = require("selenium-webdriver/testing");
 const webdriver = require("selenium-webdriver");
+const firefox = require('selenium-webdriver/firefox');
 const By = webdriver.By;
 
 let browser;
@@ -12,7 +13,7 @@ let browser;
 
 // Test suite
 test.describe("Multipage", function() {
-    test.beforeEach(function(done) {
+    beforeEach(function(done) {
         this.timeout(20000);
         browser = new webdriver.Builder()
             .withCapabilities(webdriver.Capabilities.firefox())
@@ -21,11 +22,11 @@ test.describe("Multipage", function() {
             .build();
             
 
-        browser.get("http://localhost:8082");
+        browser.get("http://localhost:8080");
         done();
     });
 
-    test.afterEach(function(done) {
+    afterEach(function(done) {
         browser.quit();
         done();
     });
